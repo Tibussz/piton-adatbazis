3ac1aeafcfd0b5bcaa8ccf8379d3efcf42de10ea
Csuka Tibor <csuka.tibor@diak.szbi-pg.hu>
2025-03-21 13:30:40 +0100

    mátyás király adatai

--- kiralyok.py
+++ kiralyok.py
@@ -3,14 +3,42 @@ from mysql.connector import (connection)
 cnx = connection.MySQLConnection(user='root', password='mysql',
                                  host='127.0.0.1',
                                  database='kiralyok')
-
+# tábla show
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+    print(---------------------------------)
+#uralkodok show    
+
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+    print(---------------------------------)
+#MATYI
 
 
+cursor.execute("SELECT * FROM uralkodo WHERE nev= 'I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)
+    print(---------------------------------)
+
+#matyi ev 
+
+cursor.execute("SELECT szul, hal FROM uralkodo WHERE nev = 'I. Mátyás'")
+for uralkodo in cursor:
+    print(f"Mátyás király élt: {uralkodo[0]} - {uralkodo[1]}")
+    print(---------------------------------)
 
 cnx.close()
+
+
+
+
+
+
+
+
+
 
 
