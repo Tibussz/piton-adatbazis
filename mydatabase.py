9e0a866b14ae1b9168fefed33963b756f8618559
Csuka Tibor <csuka.tibor@diak.szbi-pg.hu>
2025-03-28 13:21:35 +0100

    commentek hozzáadása

--- mydatabase.py
+++ mydatabase.py
@@ -6,13 +6,30 @@ mydb = mysql.connector.connect(
   password="mysql"
 )
 
+
 mycursor = mydb.cursor()
+
+#adatbazis
+
 DATABASE = "my_database"
 mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE}")
 
+#adatb. mutatása
 mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
 
+
+#hasznald ezt az adatb.
+
 mycursor.execute(f"USE {DATABASE}")
+
+#customers tabla letreh.
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))")
+#tablak mutatasa
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
